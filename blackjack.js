d750bae00c9aea7fc55d2ce2ae0a6ca0fc07b173
ZamarulNurddin <134351197+ZamarulNurddin@users.noreply.github.com>
2023-06-16 18:41:46 +0800

    Add files via upload

---
+++ blackjack.js
@@ -0,0 +1,90 @@
+
+let cards=[]
+let sum =0
+let hasBlackjack=false
+let isAlive=false 
+let message=""
+let messageEl = document.getElementById("message-el")
+let sumEl=document.getElementById("sum-el")
+let cardsEl=document.getElementById("cards-el")
+let player ={
+    name:"zama",
+    chips:120
+}
+let playerEl=document.getElementById("player-el")
+
+playerEl.textContent =player.name+": $"+player.chips
+ 
+function getRandomCard(){
+    let randomCard = Math.floor(Math.random()*13)+1
+
+   switch(randomCard){
+    case 1:randomCard=11
+    break
+    case 11:randomCard=10
+    break
+    case 12:randomCard=10
+    break
+    case 13:randomCard=10
+    break
+   }
+    return randomCard
+}
+
+function startGame(){
+    isAlive=true
+    hasBlackjack=false
+    let firstCard=getRandomCard()
+    let secondCard=getRandomCard()
+    cards=[firstCard,secondCard]
+    sum =firstCard+secondCard
+
+    renderGame()
+}
+
+function renderGame(){
+    cardsEl.textContent= "Cards: "
+
+    for(let i=0;cards.length>i;i++)
+    {
+    cardsEl.textContent += cards[i] + " "
+    }
+
+    sumEl.textContent= "Sum: " +sum
+    if(sum<=20){
+        message = "Do you want to draw a new card?"
+    }
+    else if(sum===21){
+        message = "Wohoo!you've got Blackjack!"
+        hasBlackjack=true
+    }
+    else{
+        message = "You're out of the game!"
+        isAlive=false
+    }
+
+    if(hasBlackjack===true){
+        player.chips+=120
+    }
+
+    if(isAlive===false)
+    {
+        player.chips-=120
+    }
+
+    playerEl.textContent=player.name+": $"+player.chips
+    
+    messageEl.textContent=message
+}
+
+    function newCard(){
+        if(isAlive === true && hasBlackjack === false)
+        {
+            let card=getRandomCard()
+            sum += card
+            cards.push(card)
+
+            renderGame()
+        }
+        
+    }
